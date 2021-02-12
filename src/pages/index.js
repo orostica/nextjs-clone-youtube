836c66c8299d45d197043588248d85442c8239f7
André Oróstica <andre.orostica@gmail.com>
2021-02-12 00:24:48 -0400

    Configuracao do Material UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone youtube</div>;
+  return (
+    <div>
+      Clone youtube com Next.js + Material UI
+      <Button color="secondary" variant="outlined">
+        Youtube
+      </Button>
+      ;
+    </div>
+  );
 }
