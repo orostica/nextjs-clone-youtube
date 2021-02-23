7a90140e34f737fb5bae21850182d0275e913ed5
André Oróstica <andre.orostica@gmail.com>
2021-02-23 01:30:44 -0400

    caminho absoluto config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+// Configuracao do caminho absoluto do next
+import Layout from 'src/components/Layout';
 
 export default function Home() {
   // Teste de commit gitkraken
